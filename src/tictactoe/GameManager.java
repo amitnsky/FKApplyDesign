dbeb9a16fd8b61327fa1c9327d5d4fde474d16f4
amitnsky <amitkumary828@gmail.com>
2020-01-25 13:16:35 +0530

    Changes: 	modified:   GameManager.java 	modified:   Player.java
     Implemented game between Human and Human

--- src/tictactoe/GameManager.java
+++ src/tictactoe/GameManager.java
@@ -1,16 +1,72 @@
 package tictactoe;
 
+import sun.rmi.runtime.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 // manages the game, starts with startGame.
 public class GameManager {
+    private Grid mGrid;
+    private List<Player> mPlayers;
+    private int mMaxPlayers;
+    private int mCurrentPlayerIndex;
+    private Scanner mScanner;
 
+    private Logger mLogger;
     // init the game, asks for no of players, corresponding code, returns true if initialisation was successful.
     private boolean init(){
-        return false;
+        mLogger = Logger.getLogger(this.getClass().getSimpleName());
+        mScanner = new Scanner(System.in);
+        mPlayers = new ArrayList<>();
+        mCurrentPlayerIndex = 0;
+        mMaxPlayers = 2;
+        mGrid = new Grid(3);
+
+        System.out.println("Choose the gaming mode: "
+                + "\n1: Human vs Human "
+                + " \n2: Human vs Machine(Not Valid till now, Choose 1) ");
+
+        int mode = 2;
+        if(!mScanner.hasNextInt()) return false;
+        mode = mScanner.nextInt();
+
+        if(!takePlayersInfo(mPlayers, 2))
+            return false;
+
+        return true;
     }
 
     // begins the game, takes no args, return void.
     public void startGame(){
        if(!init()) return;
+        mLogger.entering("Game Manager", "StartGame");
+
+        // keep playing while there is a single box left empty
+        while (mGrid.canMove()){
+            //print grid
+            mGrid.print();
+            Player currentPlayer = mPlayers.get(mCurrentPlayerIndex);
+
+            //keep asking for input from current player, unless he provides a correct input.
+            while (!currentPlayer.makeNextMove(mGrid));
+            mCurrentPlayerIndex = (mCurrentPlayerIndex + 1)%mMaxPlayers;
+
+            if(currentPlayer.isWinning(mGrid)){
+                mGrid.print();
+                System.out.println(currentPlayer.getName() + " Won!!");
+                return;
+            }else
+                continue;
+        }
+        if(!mGrid.canMove()){
+            System.out.println("Draw!!");
+            return;
+        }
+        mLogger.exiting("Game Manager", "StartGame");
+
     }
 
     // clear up things before exiting game.
@@ -18,5 +74,22 @@ public class GameManager {
 
     }
 
+    private boolean takePlayersInfo(List<Player> playerList, int nOfPlayers){
+        for (int i=0; i<nOfPlayers; i++){
 
+            String name = "Player " + i + 1;
+            Character code = (char) (i + 65);
+
+            System.out.println("Enter Name:");
+            if(mScanner.hasNext())
+                name = mScanner.next();
+
+            System.out.println("Enter Char Code:");
+            if(mScanner.hasNext())
+                code = mScanner.next().charAt(0);
+
+            playerList.add(new Player(name, code));
+        }
+        return true;
+    }
 }
--- src/tictactoe/Player.java
+++ src/tictactoe/Player.java
@@ -1,22 +1,40 @@
 package tictactoe;
 
-public class Player {
+import java.util.Scanner;
 
-    /**
-     * tell if this player can make a move further.
-     * If return is true, only then make next move otherwise match is draw.
-     */
-    private boolean canMakeNextMove() {
-        return false;
+public class Player {
+    private String name;
+    private Character moveCode;
+    private int lastMoveX;
+    private int lastMoveY;
+
+    public Player(String name, Character code){
+        this.name = name;
+        this.moveCode = code;
     }
 
-    // returns true if move was successful.
-    public boolean makeNextMove(){
+    public boolean makeNextMove(Grid grid){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(getName() + ", make a move. (row, col)");
+        lastMoveX = scanner.nextInt();
+        lastMoveY = scanner.nextInt();
+        if(!grid.setCharAt(getMoveCode(), lastMoveX, lastMoveY)){
+            return false;
+        }
         return true;
     }
 
     // Returns whether this player is winning.
-    public boolean isWinning(){
-        return false;
+    public boolean isWinning(Grid grid){
+        return grid.isWinning(getMoveCode(), lastMoveX, lastMoveY);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Character getMoveCode() {
+        return moveCode;
     }
 }
